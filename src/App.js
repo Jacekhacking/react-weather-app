fb3310fd3622b843e6d16ad026540f7884027cf2
JacekHacking <Jacekhacking@mac.com>
2022-12-12 16:34:13 -0700

    get tailwindcss working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1 className="text-3xl text-red-900">hello world!</h1>
     </div>
   );
 }
