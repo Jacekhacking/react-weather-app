99bd716d1f62e19b562511db5156e6f635355aff
JacekHacking <Jacekhacking@mac.com>
2022-12-14 13:30:56 -0700

    initial fetch working need to incorporate useQuery

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
   const [todaysWeather, setTodaysWeather] = useState("");
   const [city, setCity] = useState("chicago");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=38920dc3d2d7b05ac11436473723d742&units=imperial`
-        )
-        .catch((error) => {
-          console.log(error);
-        });
-      setCity(data);
-    };
-
-    fetchData();
-  }, []);
-  // console.log(todaysWeather);
-  // if (!todaysWeather) return null;
-
-  // console.log(todaysWeather.main.temp.toString());
-
   const searchCity = (e) => {
     e.preventDefault();
     setCity(e.target.elements.searchInput.value);
   };
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const weather = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=38920dc3d2d7b05ac11436473723d742&units=imperial`
+      ).then((response) => {
+        return response.json();
+      });
+      console.log(weather.main.temp, weather.name);
+      setTodaysWeather(`${weather.name} ${weather.main.temp}`);
+      return weather;
+    };
+
+    fetchWeather();
+  }, [city]);
 
   return (
     <div className="App bg-gray-700 min-h-screen text-white">
       <h1 className="text-3xl bg-">Weather App</h1>
+
       <section className="flex flex-col justify-center items-center ">
         <form action="submit" onSubmit={searchCity}>
           <div className="flex">
@@ -48,7 +45,7 @@ function App() {
 
       <section>
         <h2>Forecast</h2>
-        <p></p>
+        <p>{todaysWeather}</p>
       </section>
       <section>
         <h2>History</h2>
